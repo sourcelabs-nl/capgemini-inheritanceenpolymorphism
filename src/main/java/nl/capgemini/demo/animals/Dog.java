8d2b4656a21670e305a4e69182e63bd498957c01
Koehler, A (Arno) <arno.koehler@rabobank.nl>
2019-01-28 14:28:27 +0100

    Made the dog a Mammel

--- src/main/java/nl/capgemini/demo/animals/Dog.java
+++ src/main/java/nl/capgemini/demo/animals/Dog.java
@@ -1,6 +1,6 @@
 package nl.capgemini.demo.animals;
 
-public class Dog extends Animal {
+public class Dog extends Mammal {
 
     Dog(final float weightInKg, final boolean laysEggs) {
         super("Woof", weightInKg, laysEggs);
